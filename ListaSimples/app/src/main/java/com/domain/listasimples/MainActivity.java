97f3ff298bce9a4d256c7fb520cb7671acd11b8c
Mirella <mirella.lds@gmail.com>
2022-06-02 21:08:01 -0300

    Criando uma lista simples

--- ListaSimples/app/src/main/java/com/domain/listasimples/MainActivity.java
+++ ListaSimples/app/src/main/java/com/domain/listasimples/MainActivity.java
@@ -3,6 +3,14 @@ package com.domain.listasimples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +31,22 @@ public class MainActivity extends AppCompatActivity {
         androidVersion.add("Android 8 Oreo");
         androidVersion.add("Android 9.0 Pie");
         androidVersion.add("Android 10 Q");
+        androidVersion.add("Android 30");
+
+        ListView lvAndroidVersion = findViewById(R.id.lvAndroidVersion);
+
+        ArrayAdapter<String> adapterVersion = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, androidVersion);
+        lvAndroidVersion.setAdapter(adapterVersion);
+
+        lvAndroidVersion.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String versionName = androidVersion.get(i);
+
+                Toast.makeText(getApplicationContext(), "Item clicado posição: " + i + " " + versionName, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
